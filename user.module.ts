ccf903c1f6453384e344975967369e8e1640481a
saurabh-indianihub <2021.saurabh.agrawal@gmail.com>
2024-09-20 00:35:15 +0530

    checking undo

---
+++ user.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '@entities/user.entity';
+import { UserService } from './user.service';
+import { ConfigService } from '@nestjs/config';
+import { EmailService } from 'src/email/email.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UserController],
+  providers: [UserService, ConfigService, EmailService],
+  exports: [UserService]
+})
+export class UserModule {}
